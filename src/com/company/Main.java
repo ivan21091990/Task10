1f23dc76df85cff0bddf00dcddfff545f245cb59
AndreevIP <Andreev2102@inbox.ru>
2019-08-16 17:49:17 +0300

    Задача 10

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число строк в массиве и нажмите Enter: ");
+
+        int x = scanner.nextInt();
+
+        System.out.println("Введите число столбцов в массиве и нажмите Enter: ");
+        int y = scanner.nextInt();
+        int array[][] = new int[x][y];
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                System.out.println("Введите число в строку " + (i + 1) + " столбец " + (j + 1) + " и нажмите Enter: ");
+                array[i][j] = scanner.nextInt();
+            }
+        }
+        int count=1;
+
+        for (int element : array[0]) {
+//                for (int i = 0; i < x; i++) {
+//                for (int j = 0; j < y; j++) {
+                    System.out.println("Вывод " + (count++) + "-го числа, умноженного на 3:");
+                    System.out.println(element * 3);
+                }
+            }
+        }
+
+
